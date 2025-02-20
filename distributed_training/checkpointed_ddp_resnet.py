f156fc155b95da1187ac0bb9aa1444bc0fd72cf3
ajayrfhp <ajayrfhp1710@gmail.com>
2025-02-20 07:50:40 +0000

    added stuff

--- distributed_training/checkpointed_ddp_resnet.py
+++ distributed_training/checkpointed_ddp_resnet.py
@@ -11,127 +11,9 @@ import torch.distributed as dist
 from torch.utils.data.distributed import DistributedSampler
 import os
 from torch.utils.checkpoint import checkpoint
-
-
-class TinyImageNet(torch.utils.data.Dataset):
-    def __init__(self, dataset, transform=None):
-        self.dataset = dataset
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        x, y = self.dataset[idx]["image"], self.dataset[idx]["label"]
-        x = x.convert("RGB")
-        if self.transform:
-            x = self.transform(x)
-        y = torch.tensor(y, dtype=torch.int64)
-        return x, y
-
-
-class ResnetCheckpointed(nn.Module):
-    def __init__(self):
-        super(ResnetCheckpointed, self).__init__()
-        self.model = models.resnet18(pretrained=True)
-
-        # Store individual layers
-        self.conv1 = self.model.conv1
-        self.bn1 = self.model.bn1
-        self.relu = self.model.relu
-        self.maxpool = self.model.maxpool
-        self.layer1 = self.model.layer1
-        self.layer2 = self.model.layer2
-        self.layer3 = self.model.layer3
-        self.layer4 = self.model.layer4
-        self.avgpool = self.model.avgpool
-        self.fc = self.model.fc
-
-    def forward(self, x):
-        # Apply checkpointing to each layer
-        x = checkpoint(self.conv1, x)
-        x = checkpoint(self.bn1, x)
-        x = self.relu(x)  # ReLU is memory-efficient, no need to checkpoint
-        x = checkpoint(self.maxpool, x)
-        x = checkpoint(self.layer1, x)
-        x = checkpoint(self.layer2, x)
-        x = checkpoint(self.layer3, x)
-        x = checkpoint(self.layer4, x)
-        x = checkpoint(self.avgpool, x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-        return x
-
-
-def get_data(rank, world_size):
-    transform = transforms.Compose(
-        [transforms.Resize((224, 224)), transforms.ToTensor()]
-    )
-
-    tiny_imagenet = load_dataset("Maysee/tiny-imagenet", split="train")
-    tiny_imagenet_torch = TinyImageNet(tiny_imagenet, transform=transform)
-    num_classes = len(tiny_imagenet.features["label"].names)
-
-    train_sampler = DistributedSampler(
-        tiny_imagenet_torch, num_replicas=world_size, rank=rank
-    )
-    val_sampler = DistributedSampler(
-        tiny_imagenet_torch, num_replicas=world_size, rank=rank
-    )
-    train_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=1500, num_workers=2, sampler=train_sampler
-    )
-    val_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=1500, num_workers=2, sampler=val_sampler
-    )
-
-    return train_loader, val_loader
-
-
-def fit(
-    model,
-    train_loader,
-    val_loader,
-    epochs=1,
-    lr=0.001,
-    break_after_num_batches=None,
-    title="",
-    device=0,
-):
-    model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    criterion = torch.nn.CrossEntropyLoss()
-    total_times = []
-
-    for epoch in range(epochs):
-        start_time = time.time()
-        for batch_idx, batch in enumerate(train_loader):
-            inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            end_time = time.time()
-            total_times.append(end_time - start_time)
-            start_time = time.time()
-    batch_ids = np.arange(len(total_times))
-
-    total_times = np.array(total_times)
-    mean_time = np.round(np.mean(total_times), 2)
-
-    # Plot results
-    plt.figure(figsize=(10, 5))
-    plt.plot(batch_ids, total_times, label=f"Mean time: {mean_time} s")
-    plt.xlabel("Batch ID")
-    plt.ylabel("Time (s)")
-    plt.title(f"Training performance with gradient checkpointing")
-    plt.legend(loc="upper right")
-    plt.savefig(f"{device}_time.png")
-    plt.show()
-
+from utils import get_ddp_data, fit_profile
+from models import ResnetCheckpointed
+import argparse
 
 def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -144,7 +26,7 @@ def cleanup():
     dist.destroy_process_group()
 
 
-def train(rank, world_size):
+def train(rank, world_size, args):
     setup(rank, world_size)
     model = ResnetCheckpointed()
     model.to(rank)
@@ -152,26 +34,38 @@ def train(rank, world_size):
         model, device_ids=[rank], find_unused_parameters=True
     )
 
-    train_loader, val_loader = get_data(rank, world_size)
-    fit(
+    train_loader, val_loader = get_ddp_data(rank, world_size)
+    fit_profile(
         model,
         train_loader,
         val_loader,
-        epochs=1,
-        lr=0.001,
-        break_after_num_batches=None,
-        title=f"checkpointed_ddp_resnet_{rank}",
+        epochs=args.epochs,
+        lr=args.lr,
         device=rank,
+        num_steps=args.num_steps,
+        title=f"baseline_resnet_batch_size_{args.batch_size}_checkpointing_{args.enable_checkpointing}_num_gpus_{args.num_gpus}_ddp",
     )
+
     cleanup()
 
 
 if __name__ == "__main__":
-    # Set seeds for reproducibility
+    args = argparse.ArgumentParser()
+    args.add_argument("--batch_size", type=int, default=1500)
+    args.add_argument("--epochs", type=int, default=1)
+    args.add_argument("--lr", type=float, default=0.001)
+    args.add_argument("--enable_checkpointing", type=bool, default=False)
+    args.add_argument("--num_gpus", type=int, default=1)
+    args.add_argument("--num_steps", type=int, default=np.inf)
+    args.add_argument("--num_workers", type=int, default=2)
+
+    args = args.parse_args()
+
     torch.manual_seed(710)
     np.random.seed(710)
 
+
     start_time = time.time()
-    mp.spawn(train, args=(2,), nprocs=2, join=True)
+    mp.spawn(train, args=(2, args), nprocs=2, join=True)
     end_time = time.time()
     print(f"Training time: {end_time - start_time} s")
--- distributed_training/utils.py
+++ distributed_training/utils.py
@@ -8,7 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.distributed as dist
 from torch.utils.data.distributed import DistributedSampler
-from torch.profiler import record_function
 
 
 class TinyImageNet(torch.utils.data.Dataset):
@@ -33,16 +32,16 @@ def get_data(batch_size, num_workers):
         [transforms.Resize((224, 224)), transforms.ToTensor()]
     )
 
-    tiny_imagenet = load_dataset("Maysee/tiny-imagenet", split="train")
-    tiny_imagenet_torch = TinyImageNet(tiny_imagenet, transform=transform)
-
-    print(batch_size, num_workers)
+    tiny_imagenet_train = load_dataset("Maysee/tiny-imagenet", split="train")
+    tiny_imagenet_val = load_dataset("Maysee/tiny-imagenet", split="valid")
+    tiny_imagenet_torch_train = TinyImageNet(tiny_imagenet_train, transform=transform)
+    tiny_imagenet_torch_val = TinyImageNet(tiny_imagenet_val, transform=transform)
 
     train_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=batch_size, num_workers=num_workers
+        tiny_imagenet_torch_train, batch_size=batch_size, num_workers=num_workers
     )
     val_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=batch_size, num_workers=num_workers
+        tiny_imagenet_torch_val, batch_size=batch_size, num_workers=num_workers
     )
 
     return train_loader, val_loader
@@ -53,20 +52,24 @@ def get_ddp_data(rank, world_size):
         [transforms.Resize((224, 224)), transforms.ToTensor()]
     )
 
-    tiny_imagenet = load_dataset("Maysee/tiny-imagenet", split="train")
-    tiny_imagenet_torch = TinyImageNet(tiny_imagenet, transform=transform)
+    tiny_imagenet_train = load_dataset("Maysee/tiny-imagenet", split="train")
+    tiny_imagenet_val = load_dataset("Maysee/tiny-imagenet", split="valid")
+
+    tiny_imagenet_torch_train = TinyImageNet(tiny_imagenet_train, transform=transform)
+    tiny_imagenet_torch_val = TinyImageNet(tiny_imagenet_val, transform=transform)
+    
 
     train_sampler = DistributedSampler(
-        tiny_imagenet_torch, num_replicas=world_size, rank=rank
+        tiny_imagenet_torch_train, num_replicas=world_size, rank=rank
     )
     val_sampler = DistributedSampler(
-        tiny_imagenet_torch, num_replicas=world_size, rank=rank
+        tiny_imagenet_torch_val, num_replicas=world_size, rank=rank
     )
     train_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=1500, num_workers=2, sampler=train_sampler
+        tiny_imagenet_torch_train, batch_size=1500, sampler=train_sampler
     )
     val_loader = torch.utils.data.DataLoader(
-        tiny_imagenet_torch, batch_size=1500, num_workers=2, sampler=val_sampler
+        tiny_imagenet_torch_val, batch_size=1500, sampler=val_sampler
     )
 
     return train_loader, val_loader
@@ -82,14 +85,13 @@ def clear_cuda_memory():
     print(f"Allocated memory: {torch.cuda.memory_allocated() / 1024**2:.2f} MB")
     print(f"Cached memory: {torch.cuda.memory_reserved() / 1024**2:.2f} MB")
 
-
 def fit_profile(
     model,
     train_loader,
     val_loader,
     epochs=1,
     lr=0.001,
-    break_after_num_batches=None,
+    num_steps=np.inf,
     device=0,
     title="",
 ):
@@ -97,59 +99,42 @@ def fit_profile(
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = torch.nn.CrossEntropyLoss()
     total_times = []
-    torch.cuda.memory._record_memory_history(max_entries=10000)
-    with torch.profiler.profile(
-        activities=[
-            torch.profiler.ProfilerActivity.CPU,
-            torch.profiler.ProfilerActivity.CUDA,
-        ],
-        schedule=torch.profiler.schedule(wait=0, warmup=0, active=6, repeat=1),
-        record_shapes=True,
-        with_stack=True,
-        profile_memory=True,
-    ) as prof:
-        for epoch in range(epochs):
+    for epoch in range(epochs):
+        start_time = time.time()
+        for batch_idx, batch in enumerate(train_loader):
+            inputs, labels = batch
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            criterion(outputs, labels).backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            end_time = time.time()
+            total_times.append(end_time - start_time)
+
+            print(f"Training batch {batch_idx}")
+
+            if batch_idx >= num_steps:
+                break
             start_time = time.time()
-            for batch_idx, batch in enumerate(train_loader):
-                prof.step()
+        model.eval()
+        val_loss = 0
+        val_acc = 0
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(val_loader):
                 inputs, labels = batch
-                with record_function("to_device"):
-                    inputs, labels = inputs.to(device), labels.to(device)
-                with record_function("forward"):
-                    outputs = model(inputs)
-                with record_function("backward"):
-                    criterion(outputs, labels).backward()
-                with record_function("optimizer_step"):
-                    optimizer.step()
-                    optimizer.zero_grad()
-                end_time = time.time()
-                total_times.append(end_time - start_time)
-
-                print(batch_idx)
-
-                if (
-                    break_after_num_batches is not None
-                    and batch_idx >= break_after_num_batches
-                ):
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                preds = outputs.argmax(dim=1, keepdim=True)
+                val_acc += preds.eq(labels.view_as(preds)).sum().item()
+                print(f"Validation batch {batch_idx}")
+                if batch_idx >= num_steps:
                     break
-                start_time = time.time()
-            model.eval()
-            val_loss = 0
-            val_acc = 0
-            with torch.no_grad():
-                for batch_idx, batch in enumerate(val_loader):
-                    inputs, labels = batch
-                    inputs, labels = inputs.to(device), labels.to(device)
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-                    val_loss += loss.item()
-                    preds = outputs.argmax(dim=1, keepdim=True)
-                    val_acc += preds.eq(labels.view_as(preds)).sum().item()
-            val_loss /= len(val_loader)
-            val_acc /= len(val_loader)
-            print(f"epoch: {epoch}, val_loss: {val_loss}, val_acc: {val_acc}")
+        val_loss /= len(val_loader)
+        val_acc /= len(val_loader)
+        print(f"epoch: {epoch}, val_loss: {val_loss}, val_acc: {val_acc}")
 
-    prof.export_memory_timeline(f"{title}_memory.html", device="cuda:0")
     batch_ids = np.arange(len(total_times))
 
     total_times = np.array(total_times)
