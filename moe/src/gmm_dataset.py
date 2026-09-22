933aabbcda3b03603de03253e1821cc7f2f37060
ajayrfhp <ajayrfhp1710@gmail.com>
2026-09-21 18:13:18 -0700

    progress on classification dataset

--- moe/src/gmm_dataset.py
+++ moe/src/gmm_dataset.py
@@ -22,20 +22,21 @@ def generate_dataset(M, D, N, batch_size, test_size=0.2):
 
     X, Y = np.array(X), np.array(Y)
     indices = np.arange(X.shape[0])
-    shuffled_indices = np.random.shuffle(indices)
-    X, Y = X[shuffled_indices], Y[shuffled_indices]
+    np.random.shuffle(indices)
+    X, Y = X[indices], Y[indices]
 
     train_length = int(test_size * X.shape[0])
     X_train, X_test, Y_train, Y_test = X[:train_length], X[train_length:], Y[:train_length], Y[train_length:]
 
     X_train, Y_train = torch.from_numpy(X_train), torch.from_numpy(Y_train)
     X_test, Y_test = torch.from_numpy(X_test), torch.from_numpy(Y_test)
+    
 
-    assert X.shape == (M, D)
+    assert X.shape == (M, D), f"shape of X is instead {X.shape}"
     assert Y.shape == (M, )
 
     assert X_train.shape[0] == Y_train.shape[0]
-    assert X_test.shape[0] == Y_train.shape[0]
+    assert X_test.shape[0] == Y_test.shape[0]
 
     plt.scatter(X[:,0], X[:,1], c=Y)
     plt.savefig('gmm_dataset.png') 
